2ea269717ff36d06043ed87645b9c370b14d1e5a
Ori Manor <ori.manor@eko.com>
2022-06-14 14:30:01 +0300

    Change embed platform urls to new form

--- src/lib/ekoEmbed/v3.js
+++ src/lib/ekoEmbed/v3.js
@@ -1,11 +1,16 @@
 import EkoEmbedDeliveryBase from './EkoEmbedDeliveryBase';
 
 const INSTANCE_EMBED_API = '3.0';
-const DELIVERY_SRV_INSTANCE_PATH = '/instance';
+const DELIVERY_SRV_INSTANCE_PATH = '/embed';
 
 class EkoEmbedV3 extends EkoEmbedDeliveryBase {
     constructor(iframe) {
         super(INSTANCE_EMBED_API, DELIVERY_SRV_INSTANCE_PATH, iframe);
+    }
+
+    getDeliveryUrl(embedParams, options) {
+        const host = options.host ? `${options.host}` : `${options.env || ''}play.eko.com`;
+        return `https://${host}${this.embedpath}?${stringifyQueryParams(embedParams)}`;
     }
 }
 
